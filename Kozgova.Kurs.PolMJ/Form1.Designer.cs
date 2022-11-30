9fc0ee4870c8c5eb820ac8e0da610ddd94b66794
nasty1326 <nasty132627@gmail.com>
2022-11-30 14:19:11 +0700

    Добавьте файлы проекта.

---
+++ Kozgova.Kurs.PolMJ/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿
+namespace Kozgova.Kurs.PolMJ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numMut = new System.Windows.Forms.NumericUpDown();
+            this.label13 = new System.Windows.Forms.Label();
+            this.numCross = new System.Windows.Forms.NumericUpDown();
+            this.label12 = new System.Windows.Forms.Label();
+            this.numTurn = new System.Windows.Forms.NumericUpDown();
+            this.label11 = new System.Windows.Forms.Label();
+            this.numPop = new System.Windows.Forms.NumericUpDown();
+            this.label10 = new System.Windows.Forms.Label();
+            this.numGen = new System.Windows.Forms.NumericUpDown();
+            this.label9 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numMut)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCross)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numTurn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numGen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(93, 251);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 77);
+            this.button2.TabIndex = 35;
+            this.button2.Text = "Старт";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numMut
+            // 
+            this.numMut.DecimalPlaces = 2;
+            this.numMut.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numMut.Location = new System.Drawing.Point(212, 186);
+            this.numMut.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numMut.Name = "numMut";
+            this.numMut.Size = new System.Drawing.Size(86, 22);
+            this.numMut.TabIndex = 45;
+            this.numMut.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(25, 25);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(121, 16);
+            this.label13.TabIndex = 36;
+            this.label13.Text = "Число поколений";
+            // 
+            // numCross
+            // 
+            this.numCross.DecimalPlaces = 2;
+            this.numCross.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numCross.Location = new System.Drawing.Point(212, 146);
+            this.numCross.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numCross.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.numCross.Name = "numCross";
+            this.numCross.Size = new System.Drawing.Size(86, 22);
+            this.numCross.TabIndex = 44;
+            this.numCross.Value = new decimal(new int[] {
+            9,
+            0,
+            0,
+            65536});
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(25, 68);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(131, 16);
+            this.label12.TabIndex = 37;
+            this.label12.Text = "Размер популяции";
+            // 
+            // numTurn
+            // 
+            this.numTurn.Location = new System.Drawing.Point(212, 107);
+            this.numTurn.Maximum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numTurn.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numTurn.Name = "numTurn";
+            this.numTurn.Size = new System.Drawing.Size(86, 22);
+            this.numTurn.TabIndex = 43;
+            this.numTurn.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(25, 109);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(115, 16);
+            this.label11.TabIndex = 38;
+            this.label11.Text = "Размер турнира";
+            // 
+            // numPop
+            // 
+            this.numPop.Location = new System.Drawing.Point(212, 66);
+            this.numPop.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numPop.Name = "numPop";
+            this.numPop.Size = new System.Drawing.Size(86, 22);
+            this.numPop.TabIndex = 42;
+            this.numPop.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(25, 148);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(180, 16);
+            this.label10.TabIndex = 39;
+            this.label10.Text = "Вероятность скрещивания";
+            // 
+            // numGen
+            // 
+            this.numGen.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numGen.Location = new System.Drawing.Point(212, 25);
+            this.numGen.Name = "numGen";
+            this.numGen.Size = new System.Drawing.Size(86, 22);
+            this.numGen.TabIndex = 41;
+            this.numGen.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(25, 188);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(150, 16);
+            this.label9.TabIndex = 40;
+            this.label9.Text = "Вероятность мутации";
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(351, 25);
+            this.chart1.Name = "chart1";
+            series5.BorderWidth = 3;
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Color = System.Drawing.Color.Fuchsia;
+            series5.Legend = "Legend1";
+            series5.LegendText = "Минимум в популяции";
+            series5.Name = "Series1";
+            series6.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            series6.BorderWidth = 3;
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series6.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            series6.Legend = "Legend1";
+            series6.LegendText = "Среднее в популяции";
+            series6.Name = "Series2";
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(859, 441);
+            this.chart1.TabIndex = 46;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 370);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 16);
+            this.label1.TabIndex = 47;
+            this.label1.Text = "Минимальное найденное решение";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 399);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 16);
+            this.label2.TabIndex = 48;
+            this.label2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1222, 582);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numMut);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.numCross);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.numTurn);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.numPop);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.numGen);
+            this.Controls.Add(this.label9);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numMut)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCross)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numTurn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numGen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numMut;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.NumericUpDown numCross;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.NumericUpDown numTurn;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.NumericUpDown numPop;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.NumericUpDown numGen;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Kozgova.Kurs.PolMJ/Form1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kozgova.Kurs.PolMJ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double min0Pol; // максимальное значение выживаемости (близость к ответу)
+        double sredPol; // среднее значение выживаемости
+        int numberGenPol; // число поколений
+        int numberPopPol; // размер популяции
+        int numberTurnPol; // размер турнира 
+        double pcrossPol; // вероятность скрещивания
+        double pmutationPol; // вероятность мутации
+        int genNowPol; // текущее поколение
+        int sizeNewPol; // число популяции после скрещивания
+
+        const int lchrom = 2; // число генов
+        const int maxpop = 500; // максимальный размер популяции
+        const double xmax = 2;
+        const double xmin = 0;
+
+        public double[] maxmass;
+        public double[] sredmass;
+
+        public class individualPol //особь без пола
+        {
+            public bool pol;
+            public double[] chrom { get; set; } = new double[lchrom];
+            public double fitness { get; set; }  // коэффициент выживаемости (значение функции) 
+            public individualPol()
+            {
+                pol = false;
+                for (int i = 0; i < lchrom; i++)
+                {
+                    chrom[i] = -1;
+                }
+                fitness = -1;
+            }
+        }
+
+        // Создаем массивы для беcполового скрещивания
+        individualPol[] oldpopPol = new individualPol[maxpop]; // массив для старого поколения полового скрещивания
+        individualPol[] newpopPol = new individualPol[maxpop]; // массив для нового поколения полового скрещивания
+        individualPol[] intpopPol = new individualPol[maxpop]; // массив для сортировки поколения для полового скрещивания
+
+        //основная функция 
+        double objfunc(double x1, double x2)
+        {
+            return x1 * x1 + x2 * x2;
+        }
+
+        public void initpopPol()
+        {
+            Random rnd = new Random();
+            double k = 0;
+            for (int i = 0; i < numberPopPol; i++)
+            {
+                k = rnd.NextDouble();
+                if (k < 0.5)
+                {
+                    oldpopPol[i].pol = false; //мальчик
+                }
+                else
+                {
+                    oldpopPol[i].pol = true; //девочка
+                }
+
+                oldpopPol[i].chrom[0] = 0 + rnd.NextDouble() * 2;
+                oldpopPol[i].chrom[1] = 0 + rnd.NextDouble() * 2;
+                oldpopPol[i].fitness = objfunc(oldpopPol[i].chrom[0], oldpopPol[i].chrom[1]);
+            }
+        }
+
+        public void statisticsPol(individualPol[] pop)
+        {
+            min0Pol = pop[0].fitness;
+            sredPol = pop[0].fitness;
+            for (int i = 1; i < sizeNewPol; i++)
+            {
+                sredPol += pop[i].fitness;
+                if (pop[i].fitness < min0Pol)
+                {
+                    min0Pol = pop[i].fitness;
+                }
+            }
+            sredPol = sredPol / sizeNewPol;
+            chart1.Series[0].Points.AddXY(genNowPol, min0Pol);
+            chart1.Series[1].Points.AddXY(genNowPol, sredPol);
+            maxmass[genNowPol-1]=min0Pol;
+            sredmass[genNowPol-1]=sredPol;
+        }
+
+        public void selectPol()
+        {
+            int k;
+            Random rnd = new Random();
+            individualPol BestIndivid;
+
+            individualPol[] polMan = new individualPol[sizeNewPol];
+            individualPol[] polWoman = new individualPol[sizeNewPol];
+            int jM = 0; //счетчик мужчин
+            int jW = 0; //счетчик женщин
+
+            for (int i = 0; i < sizeNewPol; i++)
+            {
+                if (oldpopPol[i].pol)
+                {
+                    polWoman[jW] = oldpopPol[i];
+                    jW += 1;
+                }
+                else
+                {
+                    polMan[jM] = oldpopPol[i];
+                    jM += 1;
+                }
+            }
+
+            int l = 0;
+            
+            for (int i = 0; i < jW; i++)
+            {
+                k = rnd.Next(0, jM - 1);                
+                BestIndivid = polMan[k];
+                for (int j = 1; j < numberTurnPol; j++)
+                {
+                    k = rnd.Next(0, jM - 1);                    
+                    if (polMan[k].fitness < BestIndivid.fitness)
+                    {
+                        BestIndivid = polMan[k];
+                    }
+                }
+                intpopPol[l] = polWoman[i];
+                intpopPol[l + 1] = BestIndivid;
+                l += 2;
+            }
+            sizeNewPol = l - 1;
+        }
+
+        public double mutation(double fit)
+        {
+            Random rnd = new Random();
+            double p = rnd.NextDouble();
+            double j=0;
+            if (p < pmutationPol)
+            {
+                fit = fit - (rnd.NextDouble() / 20);
+               
+            }
+            
+            return fit;
+        }
+
+        public void crossoverPol()
+        {
+            double p = 0;
+            double per1;
+            double p1,p2;
+            Random rnd = new Random();
+            for (int i = 0; i <= sizeNewPol; i += 2)
+            {
+                per1 = rnd.NextDouble();
+                p1 = rnd.NextDouble();
+                p2 = rnd.NextDouble();
+                p = rnd.NextDouble();
+                individualPol c1 = new individualPol();
+                individualPol c2 = new individualPol();
+
+                if (p < pcrossPol)
+                {
+                    if (p1 < 0.5)
+                    {
+                        c1.pol = false;
+                    }
+                    else
+                    {
+                        c1.pol = true;
+                    }
+                    c1.chrom[0] = (1 - per1) * intpopPol[i].chrom[0] + per1 * intpopPol[i + 1].chrom[0];
+                    c1.chrom[1] = (1 - per1) * intpopPol[i].chrom[1] + per1 * intpopPol[i + 1].chrom[1];
+                    c1.fitness = objfunc(newpopPol[i].chrom[0], newpopPol[i].chrom[1]);
+
+                    if (p1 < 0.5)
+                    {
+                        c2.pol = false;
+                    }
+                    else
+                    {
+                        c2.pol = true;
+                    }
+
+                    c2.chrom[0] = mutation((1 - per1) * intpopPol[i + 1].chrom[0]) + mutation(per1 * intpopPol[i].chrom[0]);
+                    c2.chrom[1] = mutation((1 - per1) * intpopPol[i + 1].chrom[1]) + mutation(per1 * intpopPol[i].chrom[1]);
+                    c2.fitness = objfunc(newpopPol[i + 1].chrom[0], newpopPol[i + 1].chrom[1]);
+
+                    if (c1.pol) // если первый ребенок девочка
+                    {
+                        if (intpopPol[i].fitness > c1.fitness) // если 1 ребенок-девочка хуже матери
+                        {
+                            intpopPol[i] = c1;
+                        }
+                    }
+                    else // если первый ребенок мальчик 
+                    {
+                        if (intpopPol[i+1].fitness > c1.fitness) // если 1 ребенок-мальчик хуже отца
+                        {
+                            intpopPol[i+1] = c1;
+                        }
+                    }
+
+                    if (c2.pol) // если второй ребенок девочка
+                    {
+                        if (intpopPol[i].fitness > c2.fitness) // если 2 ребенок-девочка хуже матери
+                        {
+                            intpopPol[i] = c2;
+                        }
+                    }
+                    else // если второй ребенок мальчик 
+                    {
+                        if (intpopPol[i + 1].fitness > c2.fitness) // если 2 ребенок-мальчик хуже отца
+                        {
+                            intpopPol[i + 1] = c2;
+                        }
+                    }
+                    newpopPol[i] = intpopPol[i];
+                    newpopPol[i + 1] = intpopPol[i + 1];
+
+                }
+                else
+                {
+                    newpopPol[i] = intpopPol[i];
+                    newpopPol[i+1] = intpopPol[i + 1];
+                }
+
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < maxpop; i++)
+            {
+                oldpopPol[i] = new individualPol();//старое поколение популяции
+                newpopPol[i] = new individualPol();// новое поколение популяции
+                intpopPol[i] = new individualPol();// полонение для сортировки 
+            }
+
+            numberGenPol = (int)numGen.Value;
+            numberPopPol = (int)numPop.Value;
+            numberTurnPol = (int)numTurn.Value;
+            pcrossPol = (double)numCross.Value;
+            pmutationPol = (double)numMut.Value;
+            sizeNewPol = numberPopPol;
+
+            initpopPol(); //создаем первое поколение популяции
+            
+            genNowPol = 1;
+            maxmass=new double[numberGenPol];
+            sredmass=new double[numberPopPol];
+            statisticsPol(oldpopPol);
+
+            do
+            {
+                genNowPol++;
+
+                selectPol();
+                Console.WriteLine("Поколение после сортировки");
+                
+                crossoverPol();
+                Console.WriteLine("Поколение новое");
+                for (int k = 0; k < sizeNewPol; k++)
+                {
+                   // Console.WriteLine(newpopPol[k].chrom[0]);
+                   // Console.WriteLine(newpopPol[k].chrom[1]);
+                    Console.WriteLine(newpopPol[k].fitness);
+                   // Console.WriteLine(" ");
+                }
+                statisticsPol(newpopPol); // находим больший коэффициент выживаемости у поколения
+
+                for (int i = 0; i < maxpop; i++) // новое поколение превращаем в старое 
+                { oldpopPol[i] = newpopPol[i]; }
+                
+
+            } while (genNowPol < numberGenPol); // сколько раз было задано 
+
+            double v = 10000;
+            for (int i=0; i<genNowPol-1; i++)
+            {
+                if (maxmass[i] < v)
+                {
+                    v=maxmass[i];
+                }
+            }
+
+            label2.Text = v.ToString();
+
+        }
+    }
+}
